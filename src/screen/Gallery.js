e12429982fa8af9a6606930196b6473c90a11450
BibiHuebner <geffers.annika@gmail.com>
2021-12-15 19:21:53 +0100

    Router

---
+++ src/screen/Gallery.js
@@ -0,0 +1,12 @@
+//save a clicked item in a favorites list
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div>
+      <h2>My art gallery</h2>
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/screen/Home.js
@@ -0,0 +1,13 @@
+//landing page with search field
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h2>Homepage with search</h2>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Search;
